8b3916e553dd0df79ceee76b6bbaaf1fa7381d89
stu2301681003-bit <stu2301681003@uni-plovdiv.bg>
2025-11-10 18:47:26 +0200

    added comments on the singleton pattern

--- JapaneseRestaurantMVC/JapaneseRestaurantMVC/Services/LocalAIModelService.cs
+++ JapaneseRestaurantMVC/JapaneseRestaurantMVC/Services/LocalAIModelService.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 
+//Singleton Pattern - гарантира, че в цялата програма съществува само един
+//екземпляр от даден клас и предоставя глобална точка за достъп до него
+
 namespace JapaneseRestaurant.Services
 {
     public class LocalAIModelService
     {
-        private static LocalAIModelService? _instance;
+        private static LocalAIModelService? _instance;  
+        //тази променлива съхранява единствения екземпляр на услугата
+
         private static readonly object _lock = new object();
+        //нужно е, за да може две заявки да не създават два екземпляра едновременно
 
         private string _modelPath;
 
@@ -32,6 +38,8 @@ namespace JapaneseRestaurant.Services
                 return _instance;
             }
         }
+        //това свойство проверява дали _instance е null, ако е null се създава нов
+        //в противен случай се използва повторно съществуващият
 
         public string GenerateRecommendation(string input)
         {
@@ -42,6 +50,7 @@ namespace JapaneseRestaurant.Services
             else
                 return "Add extra noodles for better taste!";
         }
-
+        //този метод може да бъде извикан от controller-ите или views
+        //този код трябва да ми е AI recommendation-на, но за сега е просто if цикъл ;-;
     }
 }
